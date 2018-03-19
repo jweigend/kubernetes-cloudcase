c33755b7b7c2cca54a23033a6b1e281c96748ce3
Johannes Weigend <johannes.weigend@qaware.de>
2018-03-19 14:58:27 +0100

    Inventory path corrected

--- setup/os-ansible-install/uninstall.sh
+++ setup/os-ansible-install/uninstall.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i /home/cloud/cloud/setup/ansible/inventory -e openshift_disable_check=docker_storage,package_version ../../../openshift-ansible/playbooks/adhoc/uninstall.yml 
+ansible-playbook -i inventory -e openshift_disable_check=docker_storage,package_version ../../../openshift-ansible/playbooks/adhoc/uninstall.yml 
