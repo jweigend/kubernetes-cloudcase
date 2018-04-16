04f4dab8b43b0b5b6061590eb927cbbecc293952
Johannes Weigend <johannes.weigend@qaware.de>
2018-04-16 15:21:26 +0200

    working

--- setup/kubernetes/bin/join-nodes.sh
+++ setup/kubernetes/bin/join-nodes.sh
@@ -5,7 +5,7 @@
 #
 #
 
-JOIN_COMMAND=`grep "kubeadm join"  ../logs/kubeadm-init.log`
+JOIN_COMMAND=`grep "kubeadm join"  ${0%/*}/../logs/kubeadm-init.log`
 
 pssh -h ${0%/*}/../conf/join-nodes -t 600 -o ${0%/*}/../logs -i `echo $JOIN_COMMAND`
 
