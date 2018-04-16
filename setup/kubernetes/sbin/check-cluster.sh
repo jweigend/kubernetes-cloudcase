d166f93ee06ad71c0238263afa848ae68c04c82c
Johannes Weigend <johannes.weigend@qaware.de>
2018-04-16 12:32:07 +0200

    Dashboard

--- setup/kubernetes/sbin/check-cluster.sh
+++ setup/kubernetes/sbin/check-cluster.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+clear
 
+echo "checking SSH login on all cluster nodes ..."
 pssh -i -h ${0%/*}/../conf/cluster-nodes uptime
+
+echo "checking k8s pod health ..."
+source ${0%/*}/../conf/set-env.sh
+kubectl get pods --all-namespaces
+
+echo "done."
