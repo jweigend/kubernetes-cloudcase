af4a6743ac9d4a6c6dc6af967a7d094b7cfbbc8d
jweigend <johannes.weigend@qaware.de>
2018-05-03 14:39:37 +0000

    Test

---
+++ lxc/bin/copy-ssh-keys.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ssh-keygen
+
+NODES=5
+
+for i in $(seq 0 `expr $NODES - 1`); do
+    ssh-copy-id -i ~/.ssh/id_rsa.pub node$ip
+done
--- lxc/bin/setup-ssh.sh
+++ lxc/bin/setup-ssh.sh
@@ -5,4 +5,16 @@ NODES=5
 for i in $(seq 0 `expr $NODES - 1`); do
    echo Setting up ssh for node$i
    lxc exec node$i -- /bin/bash -c "yum -y install openssh-server && systemctl enable sshd && systemctl start sshd"
+   echo Chaing root password for node$i
+   lxc exec node$i -- /bin/bash -c "echo root:12qwasyx | chpasswd"
 done
+
+echo copy keys
+lxc exec node0 -- /bin/bash -c "ssh-keygen"
+
+yum -y install git
+
+git clone https://github.com/jweigend/kubernetes-cloudcase
+
+./kubernetes-cloudcase/util/copy-ssh-keys.sh
+
